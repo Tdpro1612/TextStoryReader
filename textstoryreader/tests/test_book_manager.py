66e20377233f173f86b1c967f4235895ea6ad22e
tdpro1612 <hangthieulong508@gmail.com>
2025-08-05 07:53:30 +0700

    "add test getbooklist of bookmanager"

---
+++ textstoryreader/tests/test_book_manager.py
@@ -0,0 +1,8 @@
+from textstoryreader.managers.book_manager import BookManager
+
+
+def test_get_book_list():
+    manager = BookManager()
+    book_list = manager.get_book_list()
+    print(f"Test result: {book_list}")
+    
