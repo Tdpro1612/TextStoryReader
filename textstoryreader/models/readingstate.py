9a4c0d7a45f5995bde1e2961d9b72b3482eb9f80
tdpro1612 <hangthieulong508@gmail.com>
2025-08-05 09:24:23 +0700

    "add dataclass chapter history"

--- textstoryreader/models/readingstate.py
+++ textstoryreader/models/readingstate.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass, field
-from typing import Any, Dict
+from dataclasses import dataclass
 
 
 @dataclass
@@ -7,4 +6,3 @@ class ReadingState:
     book_name: str
     last_chapter_order: int = 0
     last_position_in_chapter: str
-    
