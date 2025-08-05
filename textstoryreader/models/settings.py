476b8e7e6c65a97b487d58c9b9627d42feac3e78
tdpro1612 <hangthieulong508@gmail.com>
2025-08-05 09:18:02 +0700

    "add dataclass setting"

---
+++ textstoryreader/models/settings.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Settings:
+    font_name: str = "Roboto"
+    font_size: int = 24
+    text_color: str = "#000000"
+    backgroud_color: str = "#FFFFFF"
+    line_spacing: float = 1.2
