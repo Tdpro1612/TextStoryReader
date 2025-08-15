423b7023bd6310d05b67162e7a6f6c6f10f6833e
tdpro1612 <hangthieulong508@gmail.com>
2025-08-15 10:00:31 +0700

    "rename err englist"

--- textstoryreader/models/settings.py
+++ textstoryreader/models/settings.py
@@ -6,5 +6,5 @@ class Settings:
     font_name: str = "Roboto"
     font_size: int = 24
     text_color: str = "#000000"
-    backgroud_color: str = "#FFFFFF"
+    background_color: str = "#FFFFFF"
     line_spacing: float = 1.2
